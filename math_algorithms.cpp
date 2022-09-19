f392df25b01bfb9eb62145bea7471abefefeb6f8
Ali Elshorpagi <98808411+Ali-Elshorpagi@users.noreply.github.com>
2022-09-19 02:15:13 +0200

    Update math_algorithms.cpp

--- math_algorithms.cpp
+++ math_algorithms.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 typedef long long ll;
 typedef long double ld;
-typedef unsigned long double ull;
+typedef unsigned long long ull;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 typedef vector<int> vi;
