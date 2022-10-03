0ad5358252dca1c7e8d7b7dcd3bfadf0e28f3a64
Ali Elshorpagi <ali.elshorpagi11@gmail.com>
2022-10-03 20:01:44 +0200

    test

---
+++ test.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<pii> vpii;
+
+#define Mesh_Ali (ios_base::sync_with_stdio(false), cin.tie(NULL))
+#define all(v) ((v).begin()), ((v).end())
+#define sz(v) ((int)((v).size()))
+#define cl(v) ((v).clear())
+#define edl '\n'
+#define fr(i, x, n) for (ll i(x); i < n; ++i)
+#define fl(i, x, n) for (ll i(x); i >= n; --i)
+#define fc(it, v) for (auto &(it) : (v))
+
+void Solve()
+{
+}
+
+int main()
+{
+    Mesh_Ali;
+    // int tc(1);
+    // cin >> tc;
+    // while (tc--)
+    Solve();
+    return 0;
+}
