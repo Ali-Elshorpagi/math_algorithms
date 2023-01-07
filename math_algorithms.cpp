63a23ae9820ea6e7d5d4293067cc279a2efd51be
Ali Elshorpagi <ali.elshorpagi11@gmail.com>
2023-01-07 15:54:33 +0200

    modified

--- math_algorithms.cpp
+++ math_algorithms.cpp
@@ -16,6 +16,7 @@ typedef vector<vll> vvll;
 typedef vector<pii> vpii;
 typedef set<int> si;
 
+#define _CRT_SECURE_NO_DEPRECATE
 #define Mesh_Ali (ios_base::sync_with_stdio(false), cin.tie(NULL))
 #define all(v) ((v).begin()), ((v).end())
 #define sz(v) ((int)((v).size()))
@@ -398,6 +399,8 @@ void print_segmented_sieve(ll L, ll R)
 int main()
 {
     Mesh_Ali;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
 
     return 0;
 }
