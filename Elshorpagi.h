2d00981b96ef0763ec434f37cd5a958e392e47be
Ali-Elshorpagi <ali.elshorpagi11@gmail.com>
2023-12-09 06:15:43 +0200

    add macros to extrnal file'

--- Elshorpagi.h
+++ Elshorpagi.h
@@ -27,3 +27,35 @@
 #include <list>
 #include <array>
 #include <tuple>
+
+
+// macros
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<pii> vpii;
+typedef set<int> si;
+
+#define _CRT_SECURE_NO_DEPRECATE
+#define Sukuna (ios_base::sync_with_stdio(false), cin.tie(NULL))
+#define all(v) ((v).begin()), ((v).end())
+#define sz(v) ((int)((v).size()))
+#define cl(v) ((v).clear())
+#define edl '\n'
+#define fr(i, x, n) for (int i(x); i < n; ++i)
+#define fl(i, x, n) for (int i(x); i > n; --i)
+#define fc(it, v) for (auto &(it) : (v))
+#define sq(x) (x) * (x)
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define MOD 1000000007
--- math_algorithms.cpp
+++ math_algorithms.cpp
@@ -1,36 +1,6 @@
 // #include <bits/stdc++.h>
 #include "Elshorpagi.h"
 
-using namespace std;
-
-typedef long long ll;
-typedef long double ld;
-typedef unsigned long long ull;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<int> vi;
-typedef vector<char> vc;
-typedef vector<bool> vb;
-typedef vector<vi> vvi;
-typedef vector<ll> vll;
-typedef vector<vll> vvll;
-typedef vector<pii> vpii;
-typedef set<int> si;
-
-#define _CRT_SECURE_NO_DEPRECATE
-#define Sukuna (ios_base::sync_with_stdio(false), cin.tie(NULL))
-#define all(v) ((v).begin()), ((v).end())
-#define sz(v) ((int)((v).size()))
-#define cl(v) ((v).clear())
-#define edl '\n'
-#define fr(i, x, n) for (int i(x); i < n; ++i)
-#define fl(i, x, n) for (int i(x); i > n; --i)
-#define fc(it, v) for (auto &(it) : (v))
-#define sq(x) (x) * (x)
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
-#define MOD 1000000007
-
 /*-----------------MATH TIPS----------------*/
 // ceil(a / b) a, b must be double;
 // ceil(a / b) a, b are integers = ((a + b - 1) / b);
@@ -402,6 +372,7 @@ class Math_Algorithms
 {
 public:
     Math_Algorithms() { Sukuna; }
+    ~Math_Algorithms() { cout << edl << "DONE" << edl; }
     void TEST()
     {
         // test the MAs here;
@@ -417,6 +388,5 @@ int main()
     // cin >> tc;
     while (tc--)
         cout << "Case #" << tc + 1 << edl, MA.TEST();
-    cout << edl << "DONE" << edl;
     return (0);
 }
